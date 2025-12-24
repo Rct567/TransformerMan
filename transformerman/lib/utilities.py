5b15b038a74aa206145765bfb14290546d96b091
Rick Zuidhoek <rct999@gmail.com>
2025-12-24 23:28:14 +0100

    added profiler function in utilities.py

--- transformerman/lib/utilities.py
+++ transformerman/lib/utilities.py
@@ -1,5 +1,13 @@
 from __future__ import annotations
 
+
+import io
+import cProfile
+from contextlib import contextmanager
+from pathlib import Path
+import pstats
+
+
 import itertools
 import re
 import sys
@@ -9,9 +17,10 @@ from typing_extensions import TypeVar
 
 
 if TYPE_CHECKING:
-    from typing import Union
+    from typing import Union, IO
     from collections.abc import Iterable, Iterator, Sequence
     from typing_extensions import TypeAlias
+
     JSON_TYPE: TypeAlias = Union[dict[str, "JSON_TYPE"], list["JSON_TYPE"], str, int, float, bool, None]
 else:
     JSON_TYPE = Any
@@ -60,6 +69,7 @@ else:
 
 # create_slug function
 
+
 def create_slug(text: str) -> str:
     """
     Create a URL-safe slug from a text string.
@@ -92,9 +102,40 @@ def create_slug(text: str) -> str:
 
 # evenly_spaced_sample function
 
+
 def evenly_spaced_sample(lst: Sequence[T], sample_size: int) -> Sequence[T]:
     """Select evenly spaced items from a list."""
     if sample_size >= len(lst):
         return lst
     step: float = len(lst) / sample_size
     return [lst[int(i * step)] for i in range(sample_size)]
+
+
+# profiling
+
+
+@contextmanager
+def profile_context(amount: int = 40) -> Iterator[cProfile.Profile]:
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+    try:
+        yield profiler
+    finally:
+        profiler.disable()
+
+        def print_results(output: IO[Any], sort_key: pstats.SortKey) -> None:
+            ps = pstats.Stats(profiler, stream=output).sort_stats(sort_key)
+            ps.print_callers(amount)
+            output.write("\n\n-------------------------------------------------\n\n\n")
+            ps.print_stats(amount)
+            output.write("\n\n================================================\n\n\n\n")
+
+        output = io.StringIO()
+        print_results(output, pstats.SortKey.CUMULATIVE)
+        print_results(output, pstats.SortKey.TIME)
+        profiling_results = output.getvalue()
+
+        dump_file = Path(__file__).parent.parent.parent / "profiling_results.txt"
+        with dump_file.open("w", encoding="utf-8") as f:
+            f.write(profiling_results)
